14d0a47b5cf7d72d1807a239f62a9129dea5bb44
Zoe-Hey <150931346+Zoe-Hey@users.noreply.github.com>
2024-06-25 10:56:32 +0200

    button functionality

--- Part3/MainWindow.xaml.cs
+++ Part3/MainWindow.xaml.cs
@@ -20,5 +20,35 @@ namespace Part3
         {
             InitializeComponent();
         }
+
+        //menu buttons
+
+        private void RecAdd_Click(object sender, RoutedEventArgs e)
+        {
+            //when user clicks Add Recipe, allows tab page Add Recipe to show because it is the second index (1)
+            RecipePages.SelectedIndex = 1;  
+        }
+
+        private void RecView_Click(object sender, RoutedEventArgs e)
+        {
+            //when user clicks View Recipe, allows tab page View Recipe to show because it is the third index (2)
+            RecipePages.SelectedIndex = 2;  
+        }
+
+        private void RecFilter_Click(object sender, RoutedEventArgs e)
+        {
+            //when user clicks Filter Recipe, allows tab page Filter Recipe to show because it is the fourth index (3)
+            RecipePages.SelectedIndex = 3;  
+        }
+
+        private void BackToMenu_Click(object sender, RoutedEventArgs e)
+        {
+            //when user clicks Back To Menu on other pages, allows Menu page to show because it is the first index (0)
+            RecipePages.SelectedIndex = 0;  
+        }
+
+        //add recipe buttons
+
+
     }
 }
