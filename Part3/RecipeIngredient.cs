fff40d9cf474f3a4c933009a16704e7e4c91c277
Zoe-Hey <150931346+Zoe-Hey@users.noreply.github.com>
2024-06-25 11:18:06 +0200

    initialize variables to be accessed and put in a list to save values

--- Part3/RecipeIngredient.cs
+++ Part3/RecipeIngredient.cs
@@ -8,5 +8,24 @@ namespace Part3
 {
     class RecipeIngredient
     {
+        //ingredient class
+        public class Ingredients
+        {
+            //initialize variables with get and set methods to be accessed
+            public string IngName { get; set; }
+            public double Quantity { get; set; }
+            public string Unit { get; set; }
+            public int Calories { get; set; }
+            public string FoodGroup { get; set; }
+        }
+
+        //recipe class
+        public class Recipe
+        {
+            //ingredients and steps data saved in the generic collection: list 
+            public string RecName { get; set; }
+            public List<Ingredients> Ingredients { get; set;} = new List<Ingredients>();
+            public List<string> Steps { get; set; } = new List<string>();
+        }
     }
 }
